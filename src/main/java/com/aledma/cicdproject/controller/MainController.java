4efe745a655a72b4ec88f9e2603e80c8b80386c8
Jeoongu <ms235280@naver.com>
2024-04-04 18:22:22 +0900

    [chore] : return값 수정

--- src/main/java/com/aledma/cicdproject/controller/MainController.java
+++ src/main/java/com/aledma/cicdproject/controller/MainController.java
@@ -8,7 +8,10 @@ public class MainController {
 
     @GetMapping("/")
     public String hello(){
-        return "안녕하세요, CI / CD 프로젝트입니다.";
+        return "안녕하세요, CI / CD 프로젝트입니다.\n" +
+                "첫 workflow 파일 성공 및 완성했습니다.\n" +
+                "docker-compose를 사용했습니다.\n" +
+                "nginx도 사용해보겠습니다.";
     }
 }
 
