31fc973187add24fb012ccbfa75081f22e92b39b
Jeoongu <ms235280@naver.com>
2024-06-15 17:30:06 +0900

    [fix] : get post로 수정

--- src/main/java/com/aledma/cicdproject/controller/OAuthController.java
+++ src/main/java/com/aledma/cicdproject/controller/OAuthController.java
@@ -5,10 +5,7 @@ import com.aledma.cicdproject.service.dto.response.InfoResponseDto;
 import com.aledma.cicdproject.service.dto.response.KakaoUserInfoResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RequestMapping("/api/oauth")
@@ -16,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OAuthController {
     private final OAuthService oAuthService;
 
-    @GetMapping("/callback")
+    @PostMapping("/callback")
     public ResponseEntity<InfoResponseDto> callback(@RequestParam("code") String code){
         String accessToken = oAuthService.getAccessToken(code);
         return ResponseEntity.ok(oAuthService.getUserInfo(accessToken));
